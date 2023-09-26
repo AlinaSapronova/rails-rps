efa30481e309a8d9d099b31cf7034c93d248a1be
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-09-26 16:00:55 +0000

    finished with rock paper scissors

--- app/controllers/rockPaperScissors_controller.rb
+++
@@ -1,8 +0,0 @@
-class RockPaperScissors < ApplicationController
-  def main
-    render ({:tamplate => "main/main"})
-  end
-  def rock 
-    
-    render ({:tamplate => "game/rock"})
-  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-get("/" {:controller => "rockPaperScissors" :action => "main"})
+get("/", {:controller => "rps", :action => "main"})
 
-get("/rock" {:controller => "rockPaperScissors" :action => "rock"})
+get("/rock", {:controller => "rps", :action => "rock"})
+
+get("/paper", {:controller => "rps", :action => "paper"})
+
+get("/scissors", {:controller => "rps", :action => "scissors"})
+
 end
